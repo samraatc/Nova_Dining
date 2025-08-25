c99cd40f226bedfe350d348733d4f788ff3d4493
Bishnu001 <97343961+samraatc@users.noreply.github.com>
2025-08-25 16:32:15 +0530

    solved the cors issue

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,9 @@ const port = process.env.PORT || 5000;
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
-  'https://fortune-india.netlify.app',
-  'https://fortune-india.co.in',
+  'https://novadining.netlify.app/',
+  'https://novadining001.netlify.app/',
+  'https://adminnovadining.netlify.app/',
   ...(process.env.FRONTEND_URLS?.split(',') || []), // optional extra URLs from .env
 ];
 
